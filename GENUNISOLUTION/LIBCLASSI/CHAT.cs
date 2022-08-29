f09a4a8ac960495cba0508724e4639cea6c187b9
pietromaiorano13 <ciccidacks@gmail.com>
2022-08-29 15:01:06 +0200

    PASSAGGIO A CLASSI .NET FRAMEWORK

---
+++ GENUNISOLUTION/LIBCLASSI/CHAT.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBCLASSI
+{
+
+    public class CHAT
+    {
+        public int CHIAVE;
+        public int COD_CORSO;
+        public int COD_STUDENTE;
+        public int COD_INTERNO;
+        public string CONTENUTO;
+
+    public DataTable SelectAll()
+    {
+        SqlCommand cmd = new SqlCommand("CHAT_SELECTALL");
+        CONNESSIONE C = new CONNESSIONE();
+
+        return C.EseguiSelect(cmd);
+
+
+    }
+    public void Insert()
+    {
+        CONNESSIONE c = new CONNESSIONE();
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "CHAT_INSERT";
+        cmd.Parameters.AddWithValue("@Cod_Corso", COD_CORSO );
+        cmd.Parameters.AddWithValue("@Cod_Studete", COD_STUDENTE);
+        cmd.Parameters.AddWithValue("@Cod_Interno", COD_INTERNO);
+        cmd.Parameters.AddWithValue("@Contenuto", CONTENUTO);
+
+
+        c.EseguiSpcmdpparam(cmd);
+        //c.EseguiCommand("insert into tabTipiSpese values('" + descrizione + "')");
+
+    }
+        public DataTable SelectOne()
+        {
+            CONNESSIONE c = new CONNESSIONE();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "CHAT_SELECTONE";
+            cmd.Parameters.AddWithValue("@CHIAVE", CHIAVE);
+
+            return c.EseguiSpselectparam(cmd);
+        }
+    }
+}
---
+++ GENUNISOLUTION/LIBCLASSI/RISPOSTE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBCLASSI
+{
+    public class RISPOSTE
+    {
+        public int CHIAVE;
+        public int COD_STUDENTE;
+        public int COD_TEST;
+        public string RISPOSTA;
+
+    }
+}
