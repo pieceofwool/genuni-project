d54285017c9e396fef0b3e5965db0c469a4ac98b
pieceofwool <fabioghide98@gmail.com>
2022-08-30 12:20:00 +0200

    ws
    ws

---
+++ GENUNISOLUTION/WEBSERVICE/App_Code/WSpreiscrizione.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Descrizione di riepilogo per WSpreiscrizione
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Per consentire la chiamata di questo servizio Web dallo script utilizzando ASP.NET AJAX, rimuovere il commento dalla riga seguente. 
+// [System.Web.Script.Services.ScriptService]
+public class WSpreiscrizione : System.Web.Services.WebService
+{
+
+    public WSpreiscrizione()
+    {
+
+        //Rimuovere il commento dalla riga seguente se si utilizzano componenti progettati 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
