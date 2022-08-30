2fb6365d71d8ac7fbb78c81b76e8528a3a5fc6e4
raffaellacioffi <raffaellacioffi1992@gmail.com>
2022-08-30 12:45:42 +0200

    Riordinate pagine e implementata abilitazione
    va sistemato se necessario con nomi classi effettive

---
+++ GENUNISOLUTION/GENUNI/Admin/Popup/Abilita.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Abilita.aspx.cs" Inherits="Admin_Popup_Abilita" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Sei sicuro/a di voler cambiare lo stato di abilitazione dell'utente selezionato?<br />
+            <asp:Button ID="btnAbilita" runat="server" Text="Modifica stato" OnClick="btnAbilita_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ GENUNISOLUTION/GENUNI/Admin/Popup/Abilita.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Popup_Abilita : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void btnAbilita_Click(object sender, EventArgs e)
+    {
+        UTENTI U = new UTENTI();
+        int chiave= int.Parse(Session["chiave"].ToString());
+        U.Chiave= chiave;
+        U.Abilitato();
+
+        ScriptManager.RegisterClientScriptBlock(this, GetType(), "ATTENZIONE", "alert('Stato modificato correttamente')", true);
+    }
+}
