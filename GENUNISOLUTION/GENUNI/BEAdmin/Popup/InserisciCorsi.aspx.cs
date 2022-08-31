5cc9be4c33f8ae6afd8af853ac9ff18eb175091b
Roberto Ninno <RobertoNinno23@outlook.it>
2022-08-31 19:28:29 +0200

    Cleaned up after be admin update

--- GENUNISOLUTION/GENUNI/BEAdmin/Popup/InserisciCorsi.aspx.cs
+++ GENUNISOLUTION/GENUNI/BEAdmin/Popup/InserisciCorsi.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -23,28 +24,33 @@ public partial class Admin_Popup_InserisciCorsi : System.Web.UI.Page
 
         //UTENTI U = new UTENTI();
         //ddlUtenti.DataSource = U.SelectTutor();
-        //ddlUtenti.DataValueField = "Chiave";
-        //ddlUtenti.DataTextField = "CognomeNome";
-        //ddlUtenti.DataBind();
-        //ddlUtenti.SelectedValue = null;
+        ddlUtenti.DataValueField = "Chiave";
+        ddlUtenti.DataTextField = "Cognome";
+        ddlUtenti.DataBind();
+        ddlUtenti.SelectedValue = null;
     }
 
     protected void btnInserisci_Click(object sender, EventArgs e)
     {
-        //if (string.IsNullOrEmpty(txtTitolo.Text) || string.IsNullOrEmpty(txtTipo.Text) || string.IsNullOrEmpty(txtDescrizione.Text))
-        //{
-        //    ScriptManager.RegisterClientScriptBlock(this, GetType(), "ATTENZIONE", "alert('Tutti i campi devono essere pieni')", true);
-        //    return;
-        //}
+        if (string.IsNullOrEmpty(txtTitolo.Text) || string.IsNullOrEmpty(txtTipo.Text) || string.IsNullOrEmpty(txtDescrizione.Text))
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "ATTENZIONE", "alert('Tutti i campi devono essere pieni')", true);
+            return;
+        }
+        if (!estensioni.Contains(Path.GetExtension(fupAvatar.FileName)))
+        {
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Formato file non valido: caricare .jpg, .png o .bmp')", true); ;
+            return;
+        }
 
 
 
-        //int Cod_Utente = int.Parse(ddlUtenti.SelectedValue.ToString());
-        //string Titolo = txtTitolo.Text.Trim();
-        //string Tipo = txtTipo.Text.Trim();
-        //string Descrizione = txtDescrizione.Text.Trim();
-        //string Data_Partenza = txtDataPartenza.Text.Trim();
-
+        int Cod_Utente = int.Parse(ddlUtenti.SelectedValue.ToString());
+        string Titolo = txtTitolo.Text.Trim();
+        string Tipo = txtTipo.Text.Trim();
+        string Descrizione = txtDescrizione.Text.Trim();
+        string Data_Partenza = txtDataPartenza.Text.Trim();
+        byte[] Avatar = fupAvatar.FileBytes;
 
 
         //CORSI C = new CORSI();
@@ -52,12 +58,15 @@ public partial class Admin_Popup_InserisciCorsi : System.Web.UI.Page
         //C.Tipo = Tipo;
         //C.Descrizione = Descrizione;
         //C.Data_Partenza = Data_Partenza;
+        //C.Avatar = Avatar;
+        //C.TIPO_IMG = fupAvatar.PostedFile.ContentType;
         //C.Insert();
 
-        //lbl.Text = "Record Inserito";
-        //txtTitolo.Text = "";
-        //txtTipo.Text = "";
-        //txtDescrizione.Text = "";
-        //txtDataPartenza.Text = "";
+        lbl.Text = "Record Inserito";
+        txtTitolo.Text = "";
+        txtTipo.Text = "";
+        txtDescrizione.Text = "";
+        txtDataPartenza.Text = "";
+        fupAvatar.Dispose();
     }
 }
