42385290fccff4c4392bc30ad89c143c700884e6
Roberto Ninno <RobertoNinno23@outlook.it>
2022-09-02 12:14:15 +0200

    updated login

--- GENUNISOLUTION/GENUNI/Login.aspx.cs
+++ GENUNISOLUTION/GENUNI/Login.aspx.cs
@@ -11,36 +11,64 @@ public partial class login2 : System.Web.UI.Page
     {
 
     }
-    //protected void btnInvia_Click(object sender, EventArgs e)
-    //{
-    //// controlli formali
-    //if (string.IsNullOrEmpty(txtMail.Text) || string.IsNullOrEmpty(txtPassword.Text))
-    //{
-    //    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Attenzione: compilare tutti i campi!')", true);
-    //    return;
-    //}
+    protected void btnInvia_Click(object sender, EventArgs e)
+    {
+        // controlli formali
+        if (string.IsNullOrEmpty(txtMail.Text) || string.IsNullOrEmpty(txtPassword.Text))
+        {
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Attenzione: compilare tutti i campi!')", true);
+            return;
+        }
 
-    //// occhio alla pass, è criptata!! se volete fare delle prove inserendo qualcosa a mano dal DB
-    //// togliete il CRYPTA a riga 29
-    //UTENTI u = new UTENTI();
-    //u.email = txtMail.Text.Trim();
-    //u.password = CRYPTA.Crypta(txtPassword.Text.Trim());
+        // occhio alla pass, è criptata!! se volete fare delle prove inserendo qualcosa a mano dal DB
+        // togliete il CRYPTA a riga 27 e 33
 
-    //// se il login va a buon fine, reindirizzo alla pagina figlia "default.aspx
-    //// e mi salvo il tipoUtente nella session
+        UTENTI.Utenti_WSSoapClient U = new UTENTI.Utenti_WSSoapClient();
+        CRYPTA.Crypta_WSSoapClient C = new CRYPTA.Crypta_WSSoapClient();
 
-    //if (u.Login() == true)
-    //{
-    //    int codiceUtente = u.RecuperaCodUtente();
-    //    Session["tipoUtente"] = u.RecuperaTipoUtente(codiceUtente);
-    //    Session["CodiceUtente"] = codiceUtente;
-    //    Response.Redirect("GestioneLibri.aspx");
-    //}
+        string usr = txtMail.Text.Trim();
 
-    //else
-    //{
-    //    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Password o email errati o autorizzazione all'accesso negata')", true);
-    //    return;
-    //}
-    //}
+        string plaintext = txtPassword.Text.Trim();
+
+        string pwd = C.PWD_CRYPTA(plaintext);
+
+        // se il login va a buon fine, reindirizzo alla pagina figlia "default.aspx
+        // e mi salvo il tipoUtente nella session
+
+        if (U.Login(usr, pwd) == true)
+        {
+            int CodiceUtente = U.RecuperaCodUtente(usr);
+            Session["TipoAttore"] = U.TipoLogin(CodiceUtente);
+            Session["CodiceAttore"] = CodiceUtente;
+
+            char usertype = Convert.ToChar(Session["TipoAttore"]);
+
+            char testpercontrollareilvalorediusertype = usertype; // usa un punto di stop per controllare il valore di usertype
+
+            switch (usertype)
+            {
+                case 'A':
+                    Response.Redirect("../BEAdmin/"); //Inserire le pagine principali di redirect 
+                    break;
+
+                case 'T':
+                    Response.Redirect("../BETutor/"); //Inserire le pagine principali di redirect 
+                    break;
+
+                case 'C':
+                    Response.Redirect("../BEContabilita/"); //Inserire le pagine principali di redirect 
+                    break;
+
+            }
+
+        }
+
+        else
+        {
+
+
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('Password o email errati o autorizzazione all'accesso negata')", true);
+            return;
+        }
+    }
 }
