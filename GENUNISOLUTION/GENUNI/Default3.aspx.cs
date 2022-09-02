1afba7ff936ba1dda9ff10ff9c25ddb66a1e490c
Jaackvisio <giacomovis.gv@gmail.com>
2022-09-02 12:01:41 +0200

    1

--- GENUNISOLUTION/GENUNI/Default3.aspx.cs
+++ GENUNISOLUTION/GENUNI/Default3.aspx.cs
@@ -11,18 +11,32 @@ using System.Data.SqlClient;
 
 public partial class Default3 : System.Web.UI.Page
 {
+    //protected void Button1_Click(object sender, EventArgs e)
+    //{
+    //    string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+    //    string contentType = FileUpload1.PostedFile.ContentType;
+    //    using (Stream fs = FileUpload1.PostedFile.InputStream)
+    //    {
+    //        using (BinaryReader br = new BinaryReader(fs))
+    //        {
+    //            byte[] bytes = br.ReadBytes((Int32)fs.Length);
+    //            CORSI.Corsi_WSSoapClient corsi = new CORSI.Corsi_WSSoapClient();
+    //            corsi.Insert(1, filename, "pluto", "paperino", bytes, txtData.Text, contentType);
+    //        }
+    //    }
+    //}
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        CORSI.Corsi_WSSoapClient corsi = new CORSI.Corsi_WSSoapClient();
+
+
+        DataTable dt = corsi.SelectAll();
+        if (corsi != null)
+
         {
-            CORSI.Corsi_WSSoapClient corsi = new CORSI.Corsi_WSSoapClient();
-            
-
-            DataTable dt = corsi.SelectAll();
-            if (corsi != null)
-
+            foreach (DataRow corsi1 in dt.Rows)
             {
-                foreach (DataRow corsi1 in dt.Rows)
-                {
                 //    byte[] bytes = corsi1.Field<byte[]>("Avatar_Corso");
 
                 //    //Byte[] bytes = (Byte[])dt.Rows[2]["Avatar_Corso"];
@@ -46,35 +60,22 @@ public partial class Default3 : System.Web.UI.Page
 
                 //    Response.End();
 
-                    string tipo = corsi1.Field<string>("Tipo_Img").ToString();
+                string tipo = corsi1.Field<string>("Tipo_Img").ToString();
 
-                    byte[] arr1 = corsi1.Field<byte[]>("Avatar_Corso");
-                    //byte[] arr1 = (byte[])dt.Rows[0]["Avatar"];
-                    if (arr1 != null)
-                    {
-                        string base64String1 = Convert.ToBase64String(arr1, 0, arr1.Length);
-                        string Src = "data:image/" + tipo + ";base64," + base64String1;
-                        lit.Text += "<img style='width:200px' src='" + Src + "' />";
-                    }
-
+                byte[] arr1 = corsi1.Field<byte[]>("Avatar_Corso");
+                //byte[] arr1 = (byte[])dt.Rows[0]["Avatar"];
+                if (arr1 != null)
+                {
+                    string base64String1 = Convert.ToBase64String(arr1, 0, arr1.Length);
+                    string Src = "data:image/" + tipo + ";base64," + base64String1;
+                    lit.Text += "<img style='width:200px' src='" + Src + "' />";
                 }
 
             }
 
         }
+
+
     }
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
-        string contentType = FileUpload1.PostedFile.ContentType;
-        using (Stream fs = FileUpload1.PostedFile.InputStream)
-        {
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                CORSI.Corsi_WSSoapClient corsi = new CORSI.Corsi_WSSoapClient();
-                corsi.Insert(1, filename, "pluto", "paperino", bytes, txtData.Text, contentType);
-            }
-        }
-    }
+
 }
--- GENUNISOLUTION/GENUNI/tipologieCorsi.aspx.cs
+++ GENUNISOLUTION/GENUNI/tipologieCorsi.aspx.cs
@@ -10,5 +10,13 @@ public partial class tipologieCorsii : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        litCorso.Text = " < div class=\"col - md - 6 col - lg - 4 wow bounceInUp\" data-wow-duration=\"1.4s\">";
+        litCorso.Text += "< div class=\"box\">";
+        litCorso.Text += "< div class=\"icon\" style=\"background: #fceef3;\"><i class=\"ion-ios-analytics-outline\" style=\"color: #ff689b;\"></i></div>";
+        litCorso.Text += "< h4 class=\"title\"><a href = \"#\" > Lorem Ipsum</a></h4>";
+        litCorso.Text += "< p class=\"description\">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p></div></div>";
+       
+
+
     }
 }
