a8877789d6d5866f88acb5a5beb883be0f2fd929
Jaackvisio <giacomovis.gv@gmail.com>
2022-09-02 13:10:19 +0200

    Update tipologieCorsi.aspx.cs

--- GENUNISOLUTION/GENUNI/tipologieCorsi.aspx.cs
+++ GENUNISOLUTION/GENUNI/tipologieCorsi.aspx.cs
@@ -10,9 +10,15 @@ public partial class tipologieCorsii : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
-        DataTable dt = new DataTable();
-        dt = C.SelectAll();
+        CaricaTutti();
+
+    }
+
+    //un metodo carica corsi
+    protected void CaricaCorsi(DataTable dt)
+    {
+        litCorso.Text = "";
+
         foreach (DataRow dr in dt.Rows)
         {
             string Titolo = dr.Field<string>("Titolo").ToString();
@@ -25,22 +31,50 @@ public partial class tipologieCorsii : System.Web.UI.Page
             {
                 string base64String1 = Convert.ToBase64String(arr, 0, arr.Length);
                 Src = "data:image/" + tipo + ";base64," + base64String1;
-                 
+
             }
             else { Src = "img/logo.png"; }
 
             litCorso.Text += "<div class=\"col - md - 6 col - lg - 4 wow bounceInUp\" data-wow-duration=\"1.4s\">";
             litCorso.Text += "<div class=\"box\">";
-            litCorso.Text += "<img style=\"width:99%; border-radius:5% \" src='"+Src+"' />";
-            litCorso.Text += "<h4 class=\"title\"><a href = \"#\" >"+ Titolo +"  </a></h4>";
-            litCorso.Text += "<p class=\"description\">"+ Descrizione +" </p>";
+            litCorso.Text += "<img style=\"width:99%; border-radius:5% \" src='" + Src + "' />";
+            litCorso.Text += "<h4 class=\"title\"><a href = \"#\" >" + Titolo + "  </a></h4>";
+            litCorso.Text += "<p class=\"description\">" + Descrizione + " </p>";
             litCorso.Text += "</div>";
             litCorso.Text += "</div>";
         }
-        
-        
-       
+    }
 
 
+    //uno mostra tutti i corsi, e aggiungo tre bottoni
+
+    protected void CaricaTutti()
+    {
+        CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
+        DataTable dt = new DataTable();
+        dt = C.SelectAll();
+
+        CaricaCorsi(dt);
+    }
+    //uno mostra corsi attivi,
+
+    protected void CaricaAttivi()
+    {
+        CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
+        DataTable dt = new DataTable();
+        dt = C.SelectAll(); //select attivi
+
+        CaricaCorsi(dt);
+    }
+
+    //uno mostra corsi previsti
+
+    protected void CaricaPrevisti()
+    {
+        CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
+        DataTable dt = new DataTable();
+        dt = C.SelectAll(); //select previsti
+
+        CaricaCorsi(dt);
     }
 }
