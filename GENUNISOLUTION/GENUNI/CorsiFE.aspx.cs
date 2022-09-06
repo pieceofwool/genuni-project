793583e38114b8f9e3cb485cb7fa4c5b7e620b07
Jaackvisio <giacomovis.gv@gmail.com>
2022-09-06 11:37:48 +0200

    Update CorsiFE.aspx.cs

--- GENUNISOLUTION/GENUNI/CorsiFE.aspx.cs
+++ GENUNISOLUTION/GENUNI/CorsiFE.aspx.cs
@@ -30,6 +30,7 @@ public partial class Default2 : System.Web.UI.Page
             string Titolo = dr.Field<string>("Titolo").ToString();
             string Descrizione = dr.Field<string>("Descrizione").ToString();
             string tipo = dr.Field<string>("Tipo_Img").ToString();
+            string data = dr.Field<string>("Data_Partenza").ToString();
             byte[] arr = dr.Field<byte[]>("Avatar_Corso");
             int Codice = dr.Field<int>("Chiave");
             string Src;
@@ -47,16 +48,18 @@ public partial class Default2 : System.Web.UI.Page
             //creo ogni scheda del corso
             litCorso.Text += "<div class=\"col - md - 6 col - lg - 4 wow bounceInUp\" data-wow-duration=\"1.4s\">";
             litCorso.Text += "<div class=\"box\">";
-            litCorso.Text += "<img style=\"width:250px; border-radius:5% \" src='" + Src + "' />";
-            litCorso.Text += "<h4 class=\"title\"><a href = \"#\" >" + Titolo + "  </a></h4>";
-            litCorso.Text += "<p class=\"description\">" + Descrizione + " </p>";
-            litCorso.Text += "<p id=\"CodiceCorso\" runat=\"server\" >" + Codice + "</p>";
+            litCorso.Text += "<img style=\"height:150px; border-radius:5% \" src='" + Src + "' />";
+            litCorso.Text += "<h4 class=\"title\"><a href = \"#\" ></br>" + Titolo + "  </a></h4>";
+
+            //litCorso.Text += "<p class=\"description\">" + Descrizione + " </p>";
+           
+            //litCorso.Text += "<p id=\"CodiceCorso\" runat=\"server\" >" + Codice + "</p>";
 
             //provo con la modal
             litCorso.Text += "<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModalCenter" + i + "\">Info Corso</button>";
 
             //stampo link con funzione javascript
-            litCorso.Text += "<a href=\"#\" onclick=\"popupCenter({ url: 'InfoCorso.aspx?codice=" + Codice + "', title: 'Info Corso', w: 600, h: 300})\">Info Corso</a>";
+            //litCorso.Text += "<a href=\"#\" onclick=\"popupCenter({ url: 'InfoCorso.aspx?codice=" + Codice + "', title: 'Info Corso', w: 600, h: 300})\">Info Corso</a>";
             
             litCorso.Text += "</div>";
             litCorso.Text += "</div>";
@@ -66,15 +69,18 @@ public partial class Default2 : System.Web.UI.Page
             litModalCorso.Text += "<div class=\"modal-dialog modal-dialog-centered\" role=\"document\">";
             litModalCorso.Text += "<div class=\"modal-content\">";
             litModalCorso.Text += "<div class=\"modal-header\">";
-            litModalCorso.Text += "<h5 class=\"modal-title\" id=\"exampleModalLongTitle"+i+"\">"+ Titolo +"</h5>";
+            litModalCorso.Text += "<h5 style=\"font-size: 25px;\" class=\"modal-title\" id=\"exampleModalLongTitle" + i+"\"><b>"+ Titolo +"</b></h5>";
             litModalCorso.Text += "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">";
             litModalCorso.Text += "<span aria-hidden=\"true\">&times;</span>";
             litModalCorso.Text += "</button>";
             litModalCorso.Text += "</div>";
             litModalCorso.Text += "<div class=\"modal-body\">";
 
-            //qui ci metto il contenuto ovvero la tabella per esempio i
-            litModalCorso.Text += i + "<img style=\"width:250px; border-radius:5% \" src='" + Src + "' />";
+            //qui ci metto il contenuto ovvero la tabella
+            //litModalCorso.Text +="<img style=\"width:50px; border-radius:5% \" src='" + Src + "' />";
+
+            litModalCorso.Text += "<div style=\"font-size: 18px; background-image: linear-gradient(to bottom, rgba(21, 56, 76, 0.7), rgba(0, 0, 0, 0.8)),url('" + Src+"');"
+                                  +"width:100%; min-height: 400px;background-size:cover;color:white; padding:20px; ;\">Data d'inizio: "+data+ "</br></br>Descrizione del corso:</br>" + Descrizione+"</div>";
 
             litModalCorso.Text += "</div>";
             litModalCorso.Text += "<div class=\"modal-footer\">";
