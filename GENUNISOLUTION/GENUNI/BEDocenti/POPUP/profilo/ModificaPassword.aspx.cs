5bcbddce8f3b2bb55a14ddd3f41285147b051394
AnnaVizitu <anna.vizitiu94@libero.it>
2022-09-06 18:34:57 +0200

    Aggiornamento 18:34

--- GENUNISOLUTION/GENUNI/BEDocenti/POPUP/profilo/ModificaPassword.aspx.cs
+++ GENUNISOLUTION/GENUNI/BEDocenti/POPUP/profilo/ModificaPassword.aspx.cs
@@ -55,7 +55,7 @@ public partial class POPUP_profilo_ModificaPassword : System.Web.UI.Page
             return;
         }
 
-        E.UpdatePassword(CHIAVE, password);
+        //E.UpdatePassword(CHIAVE, password);
 
 
         ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "ATTENZIONE", "alert('La password è stata modificata con successo" + "')", true); 
