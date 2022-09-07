8ab63e9d0b782d76a7a403b87230ba31925de3e1
Jaackvisio <giacomovis.gv@gmail.com>
2022-09-07 23:33:38 +0200

    fix

--- GENUNISOLUTION/GENUNI/BEDocenti/Profilo.aspx.cs
+++ GENUNISOLUTION/GENUNI/BEDocenti/Profilo.aspx.cs
@@ -29,7 +29,7 @@ public partial class Default2 : System.Web.UI.Page
 
     protected void CaricaProfilo()
     {
-        litCV.Text = "";
+        Lit2.Text = "";
 
         ESTERNI.Esterni_WSSoapClient E = new ESTERNI.Esterni_WSSoapClient();
         //int CHIAVE = int.Parse(Session["CodiceAttore"].ToString());
@@ -99,7 +99,7 @@ public partial class Default2 : System.Web.UI.Page
             embed += "If you are unable to view file, you can download from <a href = \"{0}{1}&download=1\">here</a>";
             embed += " or download <a target = \"_blank\" href = \"http://get.adobe.com/reader/\">Adobe PDF Reader</a> to view the file.";
             embed += "</object>";
-            litCV.Text = string.Format(embed, ResolveUrl("GestoreCvPdf.ashx?chiave="), CHIAVE /*valore chiave*/);
+            Lit2.Text = string.Format(embed, ResolveUrl("GestoreCvPdf.ashx?chiave="), CHIAVE /*valore chiave*/);
             
         }
         
