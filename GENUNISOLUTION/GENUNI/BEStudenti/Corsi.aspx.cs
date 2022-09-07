b353e89885d2985b3c97937ce2ccb70fbeae58e8
MattiaSal02 <matty.11salerno@gmail.com>
2022-09-07 17:03:26 +0200

    correzione errore

--- GENUNISOLUTION/GENUNI/BEStudenti/Corsi.aspx.cs
+++ GENUNISOLUTION/GENUNI/BEStudenti/Corsi.aspx.cs
@@ -50,21 +50,21 @@ public partial class BEstudenti_Default2 : System.Web.UI.Page
 
     protected void btnAttestato_Click(object sender, EventArgs e)
     {
-        System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-        HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create("https://www.canva.com/design/play?type=TACTmE1fsnQ&template=EADmBlGOtoU&category=tACZCk6N0I4&schema=web-2&locale=it-IT/attestato.pdf");
-        httpRequest.Method = WebRequestMethods.Http.Get;
-        HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-        Stream ResponseStream = httpResponse.GetResponseStream();
+        //System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+        //HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create("https://www.canva.com/design/play?type=TACTmE1fsnQ&template=EADmBlGOtoU&category=tACZCk6N0I4&schema=web-2&locale=it-IT/attestato.pdf");
+        //httpRequest.Method = WebRequestMethods.Http.Get;
+        //HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        //Stream ResponseStream = httpResponse.GetResponseStream();
 
-        using (Stream stream = httpResponse.GetResponseStream())
-        {
-            MemoryStream ms = new MemoryStream();
-            stream.CopyTo(ms);
-            byte[] arrayByte = ms.ToArray();
-            File.WriteAllBytes("C:\\Varie\\test.pdf", arrayByte);
+        //using (Stream stream = httpResponse.GetResponseStream())
+        //{
+        //    MemoryStream ms = new MemoryStream();
+        //    stream.CopyTo(ms);
+        //    byte[] arrayByte = ms.ToArray();
+        //    File.WriteAllBytes("C:\\Varie\\test.pdf", arrayByte);
 
-        }
-        lblDownload.Text = "Download completato.";
+        //}
+        //lblDownload.Text = "Download completato.";
 
     }
 }
