7144c0bb7228f46361a9012baf0ffeabc2396814
david33737337 <86953195+david33737337@users.noreply.github.com>
2022-09-08 11:26:05 +0200

    premilo
    premilo

--- GENUNISOLUTION/GENUNI/BETutor/SITUAZIONE_CORSI/situazioneCorsi.aspx.cs
+++ GENUNISOLUTION/GENUNI/BETutor/SITUAZIONE_CORSI/situazioneCorsi.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,16 +10,45 @@ public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
-
-        int COD_TUTOR = Convert.ToInt32(Session["CodiceAttore"]);
-        GridCorsi.DataSource= C.SelectForTutor(COD_TUTOR);
-        GridCorsi.DataBind();
-
+            //disabilito i bottoni 
+            btnChat.Enabled = false;
+            btnPreparaDomande.Enabled = false;
+            btnRisultatiTest.Enabled = false;
+            btnStudenti.Enabled = false;
+            popupPreparaTest.Enabled = false;
+        
+        if (!IsPostBack == true)
+        {
+            //CARICO LA GRIGLIA
+            CORSI.Corsi_WSSoapClient C = new CORSI.Corsi_WSSoapClient();
+            int COD_TUTOR = Convert.ToInt32(Session["CodiceAttore"]);
+            GridCorsi.DataSource = C.SelectForTutor(COD_TUTOR);
+            GridCorsi.DataBind();
+        }
     }
 
     protected void grvCorsi_SelectedIndexChanged(object sender, EventArgs e)
     {
+        //abilito i bottoni
+        btnChat.Enabled = true;
+        btnStudenti.Enabled = true;
+
+        //controlli per i test e relativa abilitazione dei bottoni
+        TEST.Test_WSSoapClient T = new TEST.Test_WSSoapClient();
+        int COD_CORSO = Convert.ToInt32(GridCorsi.SelectedDataKey[0]);
+        DataTable dt = new DataTable();
+        dt = T.Test_SelectCorso(COD_CORSO);
+
+        if (dt.Rows.Count < 0)
+        {
+            popupPreparaTest.Enabled = true;
+        }
+        else
+        {
+            btnPreparaDomande.Enabled = true;
+            btnRisultatiTest.Enabled = true;
+        }
+        //creo le session
         Session["Chiave_Corso"] = GridCorsi.SelectedDataKey[0];
         Session["nomecorso"] = GridCorsi.SelectedRow.Cells[4].Text;
     }
