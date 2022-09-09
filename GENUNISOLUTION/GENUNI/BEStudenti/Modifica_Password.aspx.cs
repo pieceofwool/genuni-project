77f99b0fe99df7912f4eb819d1155fd5ae0a28ad
galatialessandro355@gmail.com <galatialessandro355@gmail.com>
2022-09-09 15:56:13 +0200

    modifica Password studenti terminata

--- GENUNISOLUTION/GENUNI/BEStudenti/Modifica_Password.aspx.cs
+++ GENUNISOLUTION/GENUNI/BEStudenti/Modifica_Password.aspx.cs
@@ -12,12 +12,12 @@ public partial class Default2 : System.Web.UI.Page
     {
         if (!IsPostBack)
         {
-            if (Session["CHIAVE"] == null)
+            if (Session["CodiceAttore"] == null)
             {
                 return;
             }
 
-            int cod = int.Parse(Session["CHIAVE"].ToString());
+            int cod = int.Parse(Session["CodiceAttore"].ToString());
             ESTERNI.Esterni_WSSoapClient E = new ESTERNI.Esterni_WSSoapClient();
             DataRow sel = E.SelectOne_Profilo_Studenti(cod).Rows[0];
 
@@ -27,7 +27,7 @@ public partial class Default2 : System.Web.UI.Page
     {
         CRYPTA.Crypta_WSSoapClient C = new CRYPTA.Crypta_WSSoapClient();
         //dichiaro le variabili
-        int Chiave = int.Parse(Session["CHIAVE"].ToString());
+        int Chiave = int.Parse(Session["CodiceAttore"].ToString());
 
         string plaintext2 = txtPassword.Text.Trim();
         string pwd = C.PWD_CRYPTA(plaintext2); // E quì vengono cryptate 
@@ -42,10 +42,16 @@ public partial class Default2 : System.Web.UI.Page
             return;
         }
 
+        //contro se le password sono uguali
+        if(txtPassword.Text == txtConfermaPassw.Text)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "Dati non validi", "alert('Errore! Password Identiche')", true);
+            return;
+        }
         //modifico la Password
         ESTERNI.Esterni_WSSoapClient E = new ESTERNI.Esterni_WSSoapClient();
 
-        E.UpdatePassword_Studenti(Chiave, pwd);
+        E.UpdatePassword_Studenti(Chiave, Confermapwd);
         lbl1.Text = "Password Modificata!";
     }
 }
